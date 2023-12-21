08d59a478228b179b42a1ef529fc8427a2274ee1
Mayur Chauhan <109025933+mayurchauhanrke@users.noreply.github.com>
2023-12-21 11:17:13 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,5 @@
+const accountId = 8336
+let accountEmail = "mayurchuhan.rke786@gmail.com"
+var accountPassword = "210160"
+accountCity = "Roorkee"
+console.table([accountEmail, accountId, accountPassword, accountCity])
