2b20d3cf2659d23c8663a05c34d86682bb8d8a13
Mayur Chauhan <109025933+mayurchauhanrke@users.noreply.github.com>
2024-01-06 22:23:27 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "786aaa"
+console.log(score)
+let tostring = Number(score)
+console.log(typeof(tostring))
+console.log(typeof(score))
+console.log(tostring)
+
+/*
+"786"=> 786
+"786aaa"=> NaN
+true => 1; false =>0
+*/
+
