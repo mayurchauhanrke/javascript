ab84980d9d526208b3118cf0f1c8e08fd300df37
Mayur Chauhan <109025933+mayurchauhanrke@users.noreply.github.com>
2024-04-27 11:42:22 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,3 +11,14 @@ console.log(tostring)
 true => 1; false =>0
 */
 
+let isLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(isLoggedIn); 
+console.log(booleanIsLoggedIn);
+// 1 => true; 0=> false;
+// "" => false
+// "mayur" => true
+
+let someNumber = 3
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
